13a0f5c9f32d02fdbea3dcd3a3c96b67e08412c3
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-02-19 04:55:07 +0600

    connected the models and added logic to them, also added controllers to the modules

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,17 @@
-export class ReviewModel {
-  _id: string;
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+  @prop()
   name: string;
+  @prop()
   title: string;
+  @prop()
   description: string;
+  @prop()
   rating: string;
+  @prop()
   createdAt: Date;
 }
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ReviewController } from './review.controller';
+import { ReviewModel } from './review.model';
 
-@Module({})
+@Module({
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ReviewModel,
+        schemaOptions: {
+          collection: 'Review',
+        },
+      },
+    ]),
+  ],
+  controllers: [ReviewController],
+})
 export class ReviewModule {}
