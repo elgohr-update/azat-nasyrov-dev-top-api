dcb37ff445a90618e145c4c8e44b22daabb1b07f
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-02-25 23:40:03 +0600

    fixed test

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -15,7 +15,6 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
-import { UserEmail } from '../decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -37,13 +36,8 @@ export class ReviewController {
     }
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
-  async getByProduct(
-    @Param('productId') productId: string,
-    @UserEmail() email: string,
-  ) {
-    console.log(email);
+  async getByProduct(@Param('productId') productId: string) {
     return this.reviewService.findByProductId(productId);
   }
 }
