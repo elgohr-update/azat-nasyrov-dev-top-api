b91b991921b3442dcef8dbb1581821d26cd057d8
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-02-28 16:55:29 +0600

    added sitemap to app

---
+++ src/sitemap/sitemap.controller.ts
@@ -0,0 +1,66 @@
+import { Controller, Get, Header } from '@nestjs/common';
+import { TopPageService } from '../top-page/top-page.service';
+import { ConfigService } from '@nestjs/config';
+import { format, subDays } from 'date-fns';
+import { Builder } from 'xml2js';
+import { CATEGORY_URL } from './sitemap.constants';
+
+@Controller('sitemap')
+export class SitemapController {
+  domain: string;
+  constructor(
+    private readonly topPageService: TopPageService,
+    private readonly configService: ConfigService,
+  ) {
+    this.domain = this.configService.get('DOMAIN') ?? '';
+  }
+
+  @Get('xml')
+  @Header('content-type', 'text/xml')
+  async sitemap() {
+    // eslint-disable-next-line
+    const formatString = 'yyyy-MM-dd\'T\'HH:mm:00.000xxx';
+
+    let res = [
+      {
+        loc: this.domain,
+        lastmod: format(subDays(new Date(), 1), formatString),
+        changereq: 'daily',
+        priority: '1.0',
+      },
+      {
+        loc: `${this.domain}/courses`,
+        lastmod: format(subDays(new Date(), 1), formatString),
+        changereq: 'daily',
+        priority: '1.0',
+      },
+    ];
+
+    const pages = await this.topPageService.findAll();
+    res = res.concat(
+      pages.map((page) => {
+        return {
+          loc: `${this.domain}${CATEGORY_URL[page.firstCategory]}/${
+            page.alias
+          }`,
+          lastmod: format(new Date(page.updatedAt ?? new Date()), formatString),
+          changereq: 'weekly',
+          priority: '0.7',
+        };
+      }),
+    );
+
+    const builder = new Builder({
+      xmldec: { version: '1.0', encoding: 'UTF-8' },
+    });
+
+    return builder.buildObject({
+      urlset: {
+        $: {
+          xmlns: 'https://www.sitemaps.org/schemas/sitemap/0/9',
+        },
+        url: res,
+      },
+    });
+  }
+}
